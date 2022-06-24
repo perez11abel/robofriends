fc05f84470f6f06cd73f511056209b0151d053aa
abel <abel_perez9@hotmail.com>
2022-06-24 14:28:20 -0500

    Updates

---
+++ precache-manifest.b51a88ab3f0004b815416ec70f4b5f46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4828919bec2521e908",
+    "url": "/robofriends/static/css/main.d3fd9a82.chunk.css"
+  },
+  {
+    "revision": "9f4828919bec2521e908",
+    "url": "/robofriends/static/js/main.9f482891.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a1fe830311fd7f7f24aa",
+    "url": "/robofriends/static/css/2.c6653f9f.chunk.css"
+  },
+  {
+    "revision": "a1fe830311fd7f7f24aa",
+    "url": "/robofriends/static/js/2.a1fe8303.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c0c4637bdd556584f543643ef7f8a29c",
+    "url": "/robofriends/index.html"
+  }
+];
